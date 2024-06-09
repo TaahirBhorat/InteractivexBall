e1892c5c31406c0dd54e85d4cdc9af13aa0af492
TaahirBhorat <taahir.bhorat3@gmail.com>
2024-06-09 22:15:50 +0200

    work4343242sdsfsdfsdfsdfssdsdff

--- Homepage.py
+++ Homepage.py
@@ -5,13 +5,23 @@ import plotly.graph_objects as go
 import plotly.io as pio
 
 logo_url = 'pics/output-onlinepngtools (1).png'
+st.sidebar.image(logo_url)
 
+
+# Display the image and text above the sidebar
+st.sidebar.markdown(
+    """
+    <div style='text-align: center; margin-top: -10px;'>
+        <h2 style='margin-bottom: 0;'>xBall</h2>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 st.set_page_config(
     page_title="xBall Interactive",
     page_icon="⚽️"
 )
 
-st.sidebar.image(logo_url)
 st.title("Main Page")
 
 st.write('Here find our Interactive tools and analysis to aid in player, recruitment, match analtysis and playstyle')
